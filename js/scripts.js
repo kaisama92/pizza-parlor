289af4b0c5ab81df0f8b7addd42a1512f0a5e15e
Kai Clausen <kaiclausen123@gmail.com>
2023-01-27 14:47:55 -0800

    fix a bug where the receipt only printed out cheese pizza receipts.

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,8 @@
 
 // Pizza Business Logic
 
-function Pizza(toppings, small) {
-  this.toppings = toppings;
+function Pizza(small) {
+  this.toppings = [];
   this.size = small;
   this.cost = 0;
 }
@@ -41,25 +41,28 @@ Pizza.prototype.sizePrice = function() {
 Pizza.prototype.receipt = function() {
   let receipt;
   if (this.toppings.length === 0) {
-    receipt = "Your " + this.size + " cheese pizza comes to a total of $" + this.cost;
+    receipt = this.name + "! Your " + this.size + " cheese pizza comes to a total of $" + this.cost;
     return receipt;
   } else
-  receipt = "Your " + this.size + " pizza with " + this.toppings[0] + " and " + this.toppings[1] + " comes to a total of $" + this.cost;
+  receipt = this.name + "! Your " + this.size + " pizza with " + this.toppings[0] + ", " + this.toppings[1] + ", and " + this.toppings[2] +  " comes to a total of $" + this.cost;
   return receipt;
 };
 
 // evalToppings Business Logic
 
 function evalToppings(top1, top2, top3) {
+  let evalArray = [];
+  console.log(top1 + top2 + top3);
   if (top1 !== "0") {
-  topArray.push(top1);
+    evalArray.push(top1);
   };
   if (top2 !== "0") {
-    topArray.push(top2);
+    evalArray.push(top2);
   };
   if (top3 !== "0") {
-    topArray.push(top3);
+    evalArray.push(top3);
   };
+  return evalArray;
 }
 
 // UI Logic
@@ -68,14 +71,25 @@ function handleSubmit(event) {
   event.preventDefault();
   let size = document.getElementById("select-pizza-size").value;
   let top1 = document.getElementById("topping1").value;
+  console.log(top1);
   let top2 = document.getElementById("topping2").value;
+  console.log(top2);
   let top3 = document.getElementById("topping3").value;
-  let topArray = [];
-  evalToppings(top1, top2, top3);
-  let newPizza = new Pizza(topArray, size);
+  console.log(top3);
+  let newPizza = new Pizza(size);
+  newPizza.toppings = evalToppings(top1, top2, top3);
+  console.log(newPizza);
+  newPizza.name = document.getElementById("person1").value;
   newPizza.toppingsPrice();
   newPizza.sizePrice();
-  newPizza.name = document.getElementById("person1").value;
+  let p = document.createElement("p");
+  let receiptDiv = document.createElement("div.receipt");
+  let body = document.querySelector("body");
+  p.append(newPizza.receipt());
+  receiptDiv.append(p);
+  body.append(receiptDiv);
+  
+  
 
 
 }
