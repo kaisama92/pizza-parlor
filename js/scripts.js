ec4e409c8ae84a36ccaee403c3cac3639619e392
Kai Clausen <kaiclausen123@gmail.com>
2023-01-27 09:53:45 -0800

    WIP- creating price prototype.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
 function Pizza([topping1, topping2], small) {
   this.toppings = [topping1, topping2];
   this.size = small;
+  this.cost = 0;
+}
+
+Pizza.prototype.price = function() {
+  this.toppings.forEach(function(element) {
+    if (element.toLowerCase() = "pepperoni"){
+      this.cost += 2;
+    } else if (element.toLowerCase() = "bacon") {
+      this.cost += 4;
+    } else 
+    this.cost += 1;
+    
+  })
 }
